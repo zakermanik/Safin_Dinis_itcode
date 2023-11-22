a788595e1578e48803729231712feba2732921c8
Dinis Safin <zakermanik322@mail.ru>
2023-11-22 17:10:14 +0500

    Добавил 3 задание

--- Lesson 3/tasks/task_03.js
+++ Lesson 3/tasks/task_03.js
@@ -4,7 +4,16 @@
 * */
 
 function getVowelsCount(str) {
-
+    let vowels = ["a", "e", "i", "u", "y"];
+    let arr = str.split('');
+    let k = 0;
+    
+    arr.forEach(b => {
+        if (vowels.includes(b)) {
+            k += 1
+        }
+    });
+    return k;
 }
 
 
